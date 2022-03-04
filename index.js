212e8ff05682690ceb1a20060d0b2236f6868956
Nikesh Maurya <nikeshinknit@gmail.com>
2022-03-04 10:19:07 +0530

    change in link

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "http://justshare-nik.herokuapp.com";
+const baseURL = "https://justshare-nik.herokuapp.com";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
